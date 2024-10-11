55d4f1384e27f3c8daa9b84b509160acb14f35a1
kencije <kenan.duvnjak@elev.ga.ntig.se>
2024-10-11 15:14:36 +0200

    Update blodgrottan.py

--- blodgrottan.py
+++ blodgrottan.py
@@ -1,21 +1,62 @@
 import os
+import random
+
+namelist = ["dongelbär" , "honk" , "bertimus" , "bang" , "meow" , "woof", "fjärilsförstörare", "sten", "pinne"]
+randomname = random.choice(namelist)
+listdifficulty = []
 
 # Funktion för att pausa och vänta på att spelaren trycker på valfri tangent
 def forsättsist():
     input("Tryck på vilken knapp som helst för att fortsätta...")
 def fortsätt():
     input("")
+class Difficulty:
+    def __init__(self, namn, multi):
+        self.namn = namn
+        self.multi = multi
+        listdifficulty.append(self)
 
-# Visa första delen av texten
+class Character:
+    def __init__(self, namn, health):
+        self.namn = namn
+        self.health = health
+        
+
+
 print("Blodgrottan")
 print("===========")
 fortsätt()
 
-# Be spelaren om sitt namn
-name = input("Vad heter du, grottman? : ")
+#Svårigheter (MULTI)
+easy = Difficulty("Noob", 0.5)
+medium = Difficulty("Pro", 1)
+hard = Difficulty("Haxer", 2)
 
-# Visa nästa del och pausa mellan varje steg
-print(f"Du är grottmannen {name}, och nu ska du strida mot grottmannen dingleberry.")
+#Karaktärer (HEALTH)
+guy_1 = Character("Honkel the 5th", 8)
+
+def choose_character():
+    your_choice2 = input("Vilken karaktär vill du välja? \n ")
+    print
+
+def choose_difficulty():
+    your_choice = input("Vilken svårighet vill du välja? \n Noob \n Pro \n Haxer ")
+
+    for diff in listdifficulty:
+        if diff.namn.lower() == your_choice.lower():
+            difficulty_name = diff.namn
+            print(f"Du valde {difficulty_name}")
+choose_difficulty()
+
+
+
+
+    
+
+name = input("Vad heter du, grottman? ")
+
+
+print(f"Du är grottmannen {name}, och nu ska du strida mot grottmannen {randomname}.")
 fortsätt()
 
 print("Ni står i en stor grotta, omgiven av hungriga ögon från stammedlemmar.")
@@ -30,7 +71,7 @@ fortsätt()
 print("Stammedlemmarna sitter runt omkring er, deras hjärtan slår hårt av förväntan.")
 fortsätt()
 
-print("Postumius gör sig redo, hans blick är hård som stål, och han förbereder sig för att anfalla.")
+print(f"{randomname} gör sig redo, hans blick är hård som stål, och han förbereder sig för att anfalla.")
 fortsätt()
 
 print("Striden kan börja när som helst.\n")
